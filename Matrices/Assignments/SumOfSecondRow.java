b875f41a7d29de0e57d5705a79eede8ef745c748
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-03 01:11:11 +0530

    Add SumOfSecondRow class to calculate second row sum

---
+++ Matrices/Assignments/SumOfSecondRow.java
@@ -0,0 +1,22 @@
+public class SumOfSecondRow {
+
+    public static int sumOfRow(int[][] matrix) {
+        int sum = 0;
+        int row = 1;
+
+        for (int j = 0; j < matrix[0].length; j++)
+            sum += matrix[row][j];
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,4,9},
+                {11,4,3},
+                {2,2,3}
+        };
+
+        System.out.println("Sum = " + sumOfRow(matrix));
+    }
+}
