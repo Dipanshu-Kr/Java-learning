20de3fd84c4dc490fa28a88f3a454a39f8157595
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-03 00:26:43 +0530

    Add MathFunctionsDemo class with various math functions

---
+++ Functions/MathFunctionsDemo.java
@@ -0,0 +1,11 @@
+public class MathFunctionsDemo {
+    public static void main(String[] args) {
+        int a = 12, b = 2, c = 25, d = -7;
+
+        System.out.println(Math.sqrt(c));
+        System.out.println(Math.min(a, b));
+        System.out.println(Math.max(b, c));
+        System.out.println(Math.abs(d));
+        System.out.println(Math.pow(a, b));
+    }
+}
