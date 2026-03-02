9c1ae9636c49b9ab1c64d7ef9f758e39bba6b65b
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-03 00:21:50 +0530

    Add PrintPrimesUpToN class to print prime numbers

---
+++ Functions/PrintPrimesUpToN.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class PrintPrimesUpToN {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static void printPrimes(int n) {
+        for (int i = 1; i <= n; i++)
+            if (isPrime(i))
+                System.out.print(i + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        printPrimes(n);
+    }
+}
