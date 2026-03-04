7b268fd819b5355a605b1a638394822713bf3bf7
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-04 14:36:47 +0530

    Add PowerOfTwo class to check power of two

---
+++ Bit-Manipulation/PowerOfTwo.java
@@ -0,0 +1,11 @@
+public class PowerOfTwo {
+
+    public static boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 16;
+        System.out.println(isPowerOfTwo(n));
+    }
+}
