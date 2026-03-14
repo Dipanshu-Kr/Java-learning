7d9be779bc8b567b84c4e2eb2ca623d77d132aeb
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-14 23:27:04 +0530

    Implement multiple inheritance with Bear class

---
+++ Oops/MultipleInheritanceDemo.java
@@ -0,0 +1,23 @@
+public class MultipleInheritanceDemo {
+
+    public static void main(String[] args) {
+
+        Bear b = new Bear();
+        b.eat();
+    }
+}
+
+interface Herbivore{
+    void eat();
+}
+
+interface Carnivore{
+    void eat();
+}
+
+class Bear implements Herbivore, Carnivore{
+
+    public void eat(){
+        System.out.println("Bear eats both grass and meat");
+    }
+}
