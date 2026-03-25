d9f9c2014f4f71b26f49c8b894ba293f8c9b14d0
Dipanshu Kumar <kumardipanshu127@gmail.com>
2026-03-25 19:52:58 +0530

    Add PrintDecreasing class to print numbers in descending order

---
+++ Recursion/PrintDecreasing.java
@@ -0,0 +1,11 @@
+public class PrintDecreasing {
+    public static void printDec(int n) {
+        if (n == 0) return;
+        System.out.print(n + " ");
+        printDec(n - 1);
+    }
+
+    public static void main(String[] args) {
+        printDec(10);
+    }
+}
